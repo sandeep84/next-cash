4815ac17e3335f8bfb6a263c2b273a1010621dc4
Sandeep Vangipuram <sandeep84@gmail.com>
2025-01-07 20:49:28 +0000

    Fixes for some Morningstar quotes

--- app/lib/account_server.ts
+++ app/lib/account_server.ts
@@ -1,13 +1,7 @@
 "use server";
 
 import { commodities } from "@prisma/client";
-import {
-  fetchAccounts,
-  fetchPrices,
-  getUUID,
-  initialiseAccounts,
-  root_account,
-} from "./account_data";
+import { fetchAccounts, fetchPrices, getUUID, initialiseAccounts, root_account } from "./account_data";
 import prisma from "./prisma";
 
 export async function getAccounts() {
@@ -56,8 +50,6 @@ export async function updatePriceList() {
 }
 
 export async function updatePrice(commodity: commodities) {
-  console.log(`Fetching latest price for ${commodity.mnemonic}`);
-
   interface PriceData {
     date: Date;
     price: number;
@@ -67,11 +59,10 @@ export async function updatePrice(commodity: commodities) {
   let price_data: PriceData | undefined = undefined;
 
   if (commodity.quote_flag && commodity.quote_source == "alphavantage") {
+    console.log(`Fetching latest price for ${commodity.mnemonic}`);
     try {
       const api_key = process.env.ALPHAVANTAGE_API_KEY;
-      const res = await fetch(
-        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${commodity.mnemonic}&apikey=${api_key}`
-      );
+      const res = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${commodity.mnemonic}&apikey=${api_key}`);
       if (res.ok) {
         let res_data = await res.json();
         price_data = {
@@ -84,44 +75,49 @@ export async function updatePrice(commodity: commodities) {
       console.log(`Error fetching price data for ${commodity.mnemonic}}`);
     }
     console.log(price_data);
-  } else if (
-    commodity.quote_flag &&
-    commodity.quote_source == "morningstaruk"
-  ) {
+  } else if (commodity.quote_flag && commodity.quote_source == "morningstaruk") {
+    console.log(`Fetching latest price for ${commodity.mnemonic}`);
     try {
-      const mstar_search_regex =
-        '<td class="msDataText searchLink"><a href="(.*?)">(.*?)</a></td><td class="msDataText searchIsin"><span>(.*)</span></td>';
+      const mstar_search_regex = '<td class="msDataText searchLink"><a href="(.*?)">(.*?)</a></td><td class="msDataText searchIsin"><span>(.*)</span></td>';
 
-      const mstar_nav_regex =
-        '<td class="line heading">NAV<span class="heading"><br />([0-9]{2}/[0-9]{2}/[0-9]{4})</span>.*([A-Z]{3}).([0-9.]+)';
+      const mstar_nav_regex = '<td class="line heading">NAV<span class="heading"><br />([0-9]{2}/[0-9]{2}/[0-9]{4})</span>.*([A-Z]{3}).([0-9.]+)';
 
-      const res = await fetch(
-        `http://www.morningstar.co.uk/uk/funds/SecuritySearchResults.aspx?search=${commodity.mnemonic}`
-      );
+      var res = await fetch(`http://www.morningstar.co.uk/uk/funds/SecuritySearchResults.aspx?search=${commodity.mnemonic}`);
       if (res.ok) {
         const matches = (await res.text()).match(mstar_search_regex);
         if (matches) {
           let next_url = matches[1];
           console.log(next_url);
 
-          const res = await fetch(`http://www.morningstar.co.uk${next_url}`);
+          res = await fetch(`http://www.morningstar.co.uk${next_url}`);
           if (res.ok) {
             console.log(`Fetched from ${next_url} OK`);
             const matches = (await res.text()).match(mstar_nav_regex);
             if (matches) {
-              console.log(`Date: ${matches[1]}`);
               var parts = matches[1].split("/");
 
               price_data = {
-                date: new Date(
-                  parseInt(parts[2], 10),
-                  parseInt(parts[1], 10) - 1,
-                  parseInt(parts[0], 10)
-                ),
+                date: new Date(parseInt(parts[2], 10), parseInt(parts[1], 10) - 1, parseInt(parts[0], 10)),
                 currency: matches[2],
                 price: parseFloat(matches[3]),
               };
               console.log(price_data);
+            } else {
+              res = await fetch(`http://www.morningstar.co.uk${next_url}&InvestmentType=SA`);
+              if (res.ok) {
+                console.log(`Fetched from ${next_url} OK`);
+                const matches = (await res.text()).match(mstar_nav_regex);
+                if (matches) {
+                  var parts = matches[1].split("/");
+
+                  price_data = {
+                    date: new Date(parseInt(parts[2], 10), parseInt(parts[1], 10) - 1, parseInt(parts[0], 10)),
+                    currency: matches[2],
+                    price: parseFloat(matches[3]),
+                  };
+                  console.log(price_data);
+                }
+              }
             }
           }
         }
