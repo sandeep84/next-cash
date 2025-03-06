6c65b997d5b807e69c55f6d88b8ff3febfaa7b40
Sandeep Vangipuram <sandeep84@gmail.com>
2025-03-06 19:12:04 +0000

    Add total value card for investments

--- app/dashboard/investments/page.tsx
+++ app/dashboard/investments/page.tsx
@@ -6,15 +6,26 @@ import { Button } from "@/app/ui/button";
 import { useEffect, useState } from "react";
 import InvestmentTable from "@/app/ui/accounts/investment_table";
 import { getInvestments } from "@/app/lib/investment_server";
+import { Card } from "@/app/ui/dashboard/cards";
+import { formatCurrency } from "@/app/lib/utils";
 
 export default function Page() {
   const [accounts, setAccounts] = useState(Array<AccountNode>());
   const [root_account, setRootAccount] = useState(new AccountNode());
+  const [total_value, setTotalValue] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
-      setAccounts(await getInvestments());
+      var accs = await getInvestments();
+      setAccounts(accs);
       setRootAccount(await getRootAccount());
+
+      var value = 0;
+      accs?.map((account) => {
+        value += account.value_in_root_commodity;
+      });
+
+      setTotalValue(value);
     };
 
     fetchData();
@@ -30,6 +41,12 @@ export default function Page() {
       >
         Update prices
       </Button>
+
+      <Card
+        title="Investments"
+        value={formatCurrency(total_value, root_account.commodity)}
+        type="collected"
+      ></Card>
 
       <InvestmentTable accounts={accounts} root_account={root_account} />
     </>
